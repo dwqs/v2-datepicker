97d0b1091b4b2b9d82e7c042536282ad4a4f4865
dwqs <461147874@qq.com>
2018-01-12 19:03:57 +0800

    feat: Complate basic feature

--- src/date-picker.vue
+++ src/date-picker.vue
@@ -6,96 +6,38 @@
         }
 
     ]">
-        <span ref="trigger" class="v2-picker-trigger" @click="handleTriggerClick">{{selected ? selected : _placeholder}}</span>
-        <i :class="['v2-trigger-icon', { 'active': shown }]"></i>
+        <span ref="trigger" :class="['v2-picker-trigger', {'empty-text': !selectedDate}]" @click="handleTriggerClick">{{selectedDate ? selectedDate : _placeholder}}</span>
+        <i :class="['v2-trigger-icon', { 'active': shown }]" v-if="trigger"></i>
         <transition name="zoom-in-top">
             <div class="v2-picker-panel-wrap" v-show="shown">
                 <div class="v2-picker-panel">
                     <div class="v2-picker-panel__header">
                         <div class="v2-picker-header__label">
-                            <span class="v2-picker-header__label-year">2018 年</span>
-                            <span class="v2-picker-header__label-month">1 月</span>
+                            <span class="v2-picker-header__label-text" v-html="formatYearMonthText()"></span>
                         </div>
                         <div class="v2-picker-header__toggle v2-picker-header__toggle-prev">
-                            <i class="v2-toggle-icon v2-toggle-icon__prev-year"></i>
-                            <i class="v2-toggle-icon v2-toggle-icon__prev-month"></i>
+                            <i class="v2-toggle-icon v2-toggle-icon__prev-year" @click="changeYear(-1)"></i>
+                            <i class="v2-toggle-icon v2-toggle-icon__prev-month" @click="changeMonth(-1)"></i>
                         </div>
                         <div class="v2-picker-header__toggle v2-picker-header__toggle-next">
-                            <i class="v2-toggle-icon v2-toggle-icon__next-month"></i>
-                            <i class="v2-toggle-icon v2-toggle-icon__next-year"></i>
+                            <i class="v2-toggle-icon v2-toggle-icon__next-month" @click="changeMonth(1)"></i>
+                            <i class="v2-toggle-icon v2-toggle-icon__next-year" @click="changeYear(1)"></i>
                         </div>
                     </div>
                     <div class="v2-picker-panel__content">
                         <div class="v2-picker-panel__week-label">
                             <span v-for="day in weekDaysLabel" :key="day" v-text="day"></span>
                         </div>
-                        <table cellspacing="0" cellpadding="0" class="v2-picker-panel__table v2-picker-panel__days-table">
-                            <tbody>
-                                <tr>
-                                    <td class="v2-picker-panel__day">1</td>
-                                    <td class="v2-picker-panel__day">2</td>
-                                    <td class="v2-picker-panel__day">1</td>
-                                    <td class="v2-picker-panel__day">2</td>
-                                    <td class="v2-picker-panel__day">1</td>
-                                    <td class="v2-picker-panel__day">2</td>
-                                    <td class="v2-picker-panel__day">1</td>
-                                </tr>
-                                <tr>
-                                    <td class="v2-picker-panel__day">1</td>
-                                    <td class="v2-picker-panel__day">2</td>
-                                    <td class="v2-picker-panel__day">1</td>
-                                    <td class="v2-picker-panel__day">2</td>
-                                    <td class="v2-picker-panel__day">1</td>
-                                    <td class="v2-picker-panel__day">2</td>
-                                    <td class="v2-picker-panel__day">1</td>
-                                </tr>
-                                <tr>
-                                    <td class="v2-picker-panel__day">1</td>
-                                    <td class="v2-picker-panel__day">2</td>
-                                    <td class="v2-picker-panel__day">1</td>
-                                    <td class="v2-picker-panel__day">2</td>
-                                    <td class="v2-picker-panel__day">1</td>
-                                    <td class="v2-picker-panel__day">2</td>
-                                    <td class="v2-picker-panel__day">1</td>
-                                </tr>
-                                <tr>
-                                    <td class="v2-picker-panel__day">1</td>
-                                    <td class="v2-picker-panel__day">2</td>
-                                    <td class="v2-picker-panel__day">1</td>
-                                    <td class="v2-picker-panel__day">2</td>
-                                    <td class="v2-picker-panel__day">1</td>
-                                    <td class="v2-picker-panel__day">2</td>
-                                    <td class="v2-picker-panel__day">1</td>
-                                </tr>
-                                <tr>
-                                    <td class="v2-picker-panel__day">1</td>
-                                    <td class="v2-picker-panel__day">2</td>
-                                    <td class="v2-picker-panel__day">1</td>
-                                    <td class="v2-picker-panel__day">2</td>
-                                    <td class="v2-picker-panel__day">1</td>
-                                    <td class="v2-picker-panel__day">2</td>
-                                    <td class="v2-picker-panel__day">1</td>
-                                </tr>
-                                <tr>
-                                    <td class="v2-picker-panel__day">1</td>
-                                    <td class="v2-picker-panel__day">2</td>
-                                    <td class="v2-picker-panel__day">1</td>
-                                    <td class="v2-picker-panel__day">2</td>
-                                    <td class="v2-picker-panel__day">1</td>
-                                    <td class="v2-picker-panel__day">2</td>
-                                    <td class="v2-picker-panel__day">1</td>
-                                </tr>
-                                <tr>
-                                    <td class="v2-picker-panel__day">1</td>
-                                    <td class="v2-picker-panel__day">2</td>
-                                    <td class="v2-picker-panel__day">1</td>
-                                    <td class="v2-picker-panel__day">2</td>
-                                    <td class="v2-picker-panel__day">1</td>
-                                    <td class="v2-picker-panel__day">2</td>
-                                    <td class="v2-picker-panel__day">1</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        <div class="v2-picker-panel__table v2-picker-panel__days-table">
+                            <div class="v2-picker-panel__table-row" v-for="(row, index) in rows" :key="index">
+                                <span v-for="cell in row" :key="cell.index"
+                                      :class="getCellClasses(cell)"
+                                      @click="selectdCurDate(cell)"
+                                    >
+                                    {{cell.text}}
+                                </span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -105,6 +47,12 @@
 
 <script>
     import locals from './locals';
+
+    import { 
+        nextDate, daysOfMonth, isDate,
+        getDaysOfMonth, getFirstDateOfMonth, getLastDateOfMonth,
+        getClearHoursTime, formatDate
+    } from './utils';
 
     export default {
         name: 'v2-datepicker',
@@ -122,7 +70,7 @@
             },
             format: {
                 type: String,
-                default: 'yyyy-MM-dd'
+                default: 'yyyy/MM/dd'
             },
             disabled: {
                 type: Boolean,
@@ -131,22 +79,24 @@
             placeholder: {
                 type: String,
                 default: '' 
+            },
+            trigger: {
+                type: Boolean,
+                default: true
             }
         },
 
         data () {
             return {
                 view: 'day',
-                selected: '',
+                selectedDate: '',
+                curDate: '',
                 shown: false,
-                days: []
+                rows: [[], [], [], [], [], [], []]
             };
         },
 
         computed: {
-            _format () {
-
-            },
             _placeholder () {
                 return this.placeholder ? this.placeholder : this.lang === 'cn' ? '选择日期' : 'Choosing date...';
             },
@@ -156,9 +106,68 @@
             }
         },
 
+        watch: {
+            curDate (val) {
+                this.initDays();
+            }
+        },
+
         methods: {
             initDays () {
+                const date = this.curDate;
 
+                const curYear = date.getFullYear();
+                const curMonth = date.getMonth();
+                const curDate = date.getDate();
+                const curDay = date.getDay();
+
+                const firstDateOfMonth = getFirstDateOfMonth(date);
+                const firstWeekDay = firstDateOfMonth.getDay();
+                const daysOfMonth = getDaysOfMonth(curYear, curMonth + 1);
+                const lastDateOfMonth = getLastDateOfMonth(date);
+                const mod = (firstWeekDay + 7) % 7;
+                
+                const panelStartDate = new Date(curYear, curMonth, firstDateOfMonth.getDate() - (mod + 7));
+                const daysOfPreMonth = getDaysOfMonth(panelStartDate.getFullYear(), panelStartDate.getMonth() + 1);
+
+                // firstWeekDay = (firstWeekDay === 0 ? 7 : firstWeekDay);
+         
+                const rows = [[], [], [], [], [], [], []];
+                const minTime = firstDateOfMonth.getTime();
+                const maxTime = lastDateOfMonth.getTime();
+                let index = 0;
+
+                for (let i = 0, l = rows.length; i < l; i++) {
+                    const row = rows[i];
+                    for (let j = 0; j < 7; j++) {
+                        const cell = {};
+                        index = i * 7 + j;
+                        const d = nextDate(panelStartDate, index);
+                        const time = d.getTime();
+                        cell.index = index;
+                        cell.text = d.getDate();
+                        cell.type = time < minTime ? 'prev-month' : time > maxTime ? 'next-month' : 'normal';
+                        cell.isToday = time === getClearHoursTime(Date.now());
+                        cell.isSelected = isDate(this.selectedDate) ? time === getClearHoursTime(new Date(this.selectedDate).getTime()) : false;
+                        cell.date = d;
+                        row.push(cell);
+                    }
+                    rows[i] = row;
+                }
+
+                this.rows = [].concat(rows);
+            },
+
+            getCellClasses (cell) {
+                const classes = ['v2-picker-panel__table-cell', 'v2-picker-panel__day'];
+                classes.push(cell.type);
+                if (cell.isToday && !this.selectedDate) {
+                    classes.push('today');
+                }
+                if (cell.isSelected) {
+                    classes.push('selected');
+                }
+                return classes.join(' ');
             },
 
             getDefPlaceholder () {
@@ -166,6 +175,34 @@
                     return this.lang === 'cn' ? '开始时间 - 结束时间' : 'Choosing date...';
                 }
                 return this.lang === 'cn' ? '选择日期' : 'Choosing date...';
+            },
+
+            formatYearMonthText () {
+                // 2018&nbsp;年&nbsp;&nbsp;1&nbsp;月
+                const d = this.curDate;
+                if (this.lang === 'cn') {
+                    return `${d.getFullYear()}&nbsp;年&nbsp;&nbsp;${d.getMonth() + 1}&nbsp;月`;
+                } else if (this.lang === 'en') {
+                    return `${d.getFullYear()}&nbsp;&nbsp;${locals[this.lang].months.original[d.getMonth()]}`;
+                }
+            },
+
+            changeMonth (delta) {
+                const d = this.curDate;
+                this.curDate = new Date(d.getFullYear(), d.getMonth() + delta, d.getDate());
+            },
+
+            changeYear (delta) {
+                const d = this.curDate;
+                this.curDate = new Date(d.getFullYear() + delta, d.getMonth(), d.getDate());
+            },
+
+            selectdCurDate (cell) {
+                this.selectedDate = formatDate(cell.date, this.format);
+                this.curDate = cell.date;
+                this.$emit('input', this.selectedDate);
+                this.$emit('change', this.selectedDate);
+                this.shown = false;
             },
 
             contains (root, target) {
@@ -200,9 +237,11 @@
         },
 
         created () {
-            this.initDays();
-            if (this.value) {
-                this.selected = this.value;
+            if (!this.value || !isDate(this.value)) {
+                this.curDate = new Date(Date.now());
+            } else {
+                this.curDate = new Date(this.value);
+                this.selectedDate = formatDate(this.curDate, this.format);
             }
         },
 
--- src/utils.js
+++ src/utils.js
@@ -5,15 +5,24 @@ export const isLeapYear = year => {
     return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 };
 
-export const daysOfMonth = (year, month) => {
+export const getDaysOfMonth = (year, month) => {
     if (isNaN(year) || isNaN(month)) {
-        throw new Error(`Parameter's value may be not a number`);
+        throw new Error(`daysOfMonth: parameter's value may be not a number`);
     }
-    return new Date(Date.UTC(year, month + 1, 0)).getUTCDate();
+
+    if ([3, 5, 8, 10].includes(month)) {
+        return 30;
+    }
+    
+    return month === 2 ? (isLeapYear(year) ? 29 : 28) : 31;
+};
+
+export const getDaysOfYear = year => {
+    return isLeapYear(year) ? 366 : 365;
 };
 
 export const isDate = date => {
-    if (date === null || date === undefined) {
+    if (!date) {
         return false;
     }
     if (isNaN(new Date(date).getTime())) {
@@ -24,6 +33,40 @@ export const isDate = date => {
   
 export const isDateObject = val => {
     return val instanceof Date;
+};
+
+export const getClearHoursTime = time => {
+    const temp = new Date(time);
+    temp.setHours(0, 0, 0, 0);
+    return temp.getTime();
+};
+
+export const getFirstDateOfMonth = date => {
+    if (isDate(date)) {
+        const temp = new Date(date.getTime());
+        temp.setDate(1);
+        temp.setHours(0, 0, 0, 0);
+        return temp;
+    }
+    throw new Error(`getFirstDateOfMonth: 1st parameter may be not a valid date`);
+};
+
+export const getLastDateOfMonth = date => {
+    if (isDate(date)) {
+        const temp = new Date(date.getTime());
+        const daysOfMonth = getDaysOfMonth(temp.getFullYear(), temp.getMonth() + 1);
+        temp.setDate(daysOfMonth);
+        temp.setHours(23, 59, 59, 999);
+        return temp;
+    }
+    throw new Error(`getLastDateOfMonth: 1st parameter may be not a valid date`);
+};
+
+export const nextDate = (date, offset = 1) => {
+    if (isDate(date)) {
+        return new Date(date.getFullYear(), date.getMonth(), date.getDate() + offset);
+    }
+    throw new Error(`nextDate: 1st parameter may be not a valid date`);
 };
 
 export const addZero = val => {
@@ -44,11 +87,11 @@ export const formatDate = (date, formatStr) => {
     str = str.replace(/M/g, date.getMonth() + 1);
     str = str.replace(/dd|DD/, addZero(date.getDate()));
     str = str.replace(/d|D/g, date.getDate());
-    str = str.replace(/hh|HH/, addZero(date.getHours()));
-    str = str.replace(/h|H/g, date.getHours());
-    str = str.replace(/mm/, addZero(date.getMinutes()));
-    str = str.replace(/m/g, date.getMinutes());
-    str = str.replace(/ss|SS/, addZero(date.getSeconds()));
-    str = str.replace(/s|S/g, date.getSeconds());
+    // str = str.replace(/hh|HH/, addZero(date.getHours()));
+    // str = str.replace(/h|H/g, date.getHours());
+    // str = str.replace(/mm/, addZero(date.getMinutes()));
+    // str = str.replace(/m/g, date.getMinutes());
+    // str = str.replace(/ss|SS/, addZero(date.getSeconds()));
+    // str = str.replace(/s|S/g, date.getSeconds());
     return str;
 };
