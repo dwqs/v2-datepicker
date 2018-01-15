cbe02b07604cd487c771411d5f37c94fe49e12f2
dwqs <461147874@qq.com>
2018-01-15 23:59:25 +0800

    refactor: Register range picker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './index.less';
 
 import DatePicker from './date-picker.vue';
+import DatePickerRange from './date-range-picker.vue';
 
 function install (Vue) {
+    Vue.component(DatePickerRange.name, DatePickerRange);
     Vue.component(DatePicker.name, DatePicker);
 }
 
