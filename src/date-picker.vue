d01c393f64a0ad136f837ca67278c0e4af926269
dwqs <461147874@qq.com>
2018-02-01 20:58:13 +0800

    feat: Support picker of shortcuts

--- src/date-picker.vue
+++ src/date-picker.vue
@@ -24,8 +24,9 @@
             <path d="M921.6 93.090909l-121.018182 0L800.581818 23.272727C800.581818 9.309091 791.272727 0 777.309091 0s-23.272727 9.309091-23.272727 23.272727L754.036364 93.090909l-218.763636 0L535.272727 23.272727C535.272727 9.309091 525.963636 0 512 0c-13.963636 0-23.272727 9.309091-23.272727 23.272727L488.727273 93.090909 274.618182 93.090909 274.618182 23.272727C274.618182 9.309091 265.309091 0 251.345455 0 237.381818 0 228.072727 9.309091 228.072727 23.272727L228.072727 93.090909 102.4 93.090909C46.545455 93.090909 0 139.636364 0 195.490909l0 721.454545C0 977.454545 46.545455 1024 102.4 1024l814.545455 0c55.854545 0 102.4-46.545455 102.4-102.4L1019.345455 195.490909C1024 139.636364 977.454545 93.090909 921.6 93.090909zM977.454545 921.6c0 32.581818-27.927273 55.854545-55.854545 55.854545L102.4 977.454545C74.472727 977.454545 46.545455 949.527273 46.545455 921.6L46.545455 195.490909C46.545455 167.563636 74.472727 139.636364 102.4 139.636364l121.018182 0 0 69.818182C228.072727 218.763636 237.381818 232.727273 251.345455 232.727273c13.963636 0 23.272727-9.309091 23.272727-23.272727L274.618182 139.636364l214.109091 0 0 69.818182C488.727273 218.763636 498.036364 232.727273 512 232.727273c13.963636 0 23.272727-9.309091 23.272727-23.272727L535.272727 139.636364l218.763636 0 0 69.818182c0 13.963636 9.309091 23.272727 23.272727 23.272727s23.272727-9.309091 23.272727-23.272727L800.581818 139.636364l121.018182 0C949.527273 139.636364 977.454545 167.563636 977.454545 195.490909L977.454545 921.6z" p-id="1942" fill="#797973"></path>
         </svg>    
         <transition name="zoom-in-top">
-            <div class="v2-picker-panel-wrap" v-show="shown">
-                <div class="v2-picker-panel">
+            <div class="v2-picker-panel-wrap" v-show="shown" :style="{minWidth: minWidth + 'px'}">
+                <short-cuts v-if="shownSideBar" :shortcuts="pickerOptions.shortcuts" @pick="handleShortcutPick"></short-cuts>
+                <div class="v2-picker-panel" :style="{marginLeft: shownSideBar ? '110px' : '0'}">
                     <div class="v2-picker-panel__header">
                         <div class="v2-picker-header__label">
                             <span class="v2-picker-header__label-text" v-html="formatYearMonthText()"></span>
@@ -67,6 +68,8 @@
         getClearHoursTime, formatDate, contains
     } from './utils';
 
+    import ShortCuts from './shortcuts';
+
     export default {
         name: 'v2-datepicker',
         props: {
@@ -91,6 +94,13 @@
             trigger: {
                 type: Boolean,
                 default: true
+            },
+
+            pickerOptions: {
+                type: Object,
+                default: () => {
+                    return {};
+                }
             }
         },
 
@@ -101,7 +111,10 @@
                 curDate: new Date(),
                 shown: false,
                 shownClear: false,
-                rows: [[], [], [], [], [], [], []]
+                rows: [[], [], [], [], [], [], []],
+
+                minWidth: 270,
+                shownSideBar: false
             };
         },
 
@@ -247,6 +260,14 @@
                     return;
                 }
                 this.shown = !this.shown;
+            },
+
+            handleShortcutPick (date) {
+                this.selectedDate = formatDate(date, this.format);
+                this.curDate = date;
+                this.$emit('input', this.selectedDate);
+                this.$emit('change', this.selectedDate);
+                this.shown = false;
             }
         },
 
@@ -255,6 +276,11 @@
                 this.setDefDate();
             }
             this.initDays();
+
+            if (this.pickerOptions.shortcuts && Array.isArray(this.pickerOptions.shortcuts) && this.pickerOptions.shortcuts.length) {
+                this.minWidth = 380;
+                this.shownSideBar = true;
+            }
         },
 
         mounted () {
@@ -263,6 +289,10 @@
 
         beforeDestroy () {
             window.document.removeEventListener('click', this.handleDocClick);
+        },
+
+        components: {
+            ShortCuts
         }
     };
 </script>
