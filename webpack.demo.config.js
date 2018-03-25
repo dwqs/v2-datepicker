6ea414957f2753248caf93012baf5393cf6f14fd
dwqs <461147874@qq.com>
2018-03-25 12:47:38 +0800

    refactor: Add webpack demo config

---
+++ webpack.demo.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const webpack = require('webpack');
+const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin');
+const os = require('os');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: path.resolve(__dirname, './gh/index')
+    },
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.less$/,
+                use: ['vue-style-loader', 'css-loader', 'postcss-loader', 'less-loader']
+                // use: ExtractTextPlugin.extract({
+                //     fallback: 'css-loader',
+                //     use: ['postcss-loader', 'less-loader']
+                // })
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.vue', '.js'],
+        modules: [path.join(__dirname, './node_modules')],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
+    plugins: [
+        // new ExtractTextPlugin({
+        //     filename: '[name].css'
+        // }),
+
+        new ParallelUglifyPlugin({
+            workerCount: os.cpus().length,
+            cacheDir: '.cache/',
+            sourceMap: true,
+            uglifyJS: {
+                compress: {
+                    warnings: false,
+                    /* eslint-disable */
+                    drop_debugger: true,
+                    drop_console: true
+                },
+                mangle: true
+            }
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './tpl.html'
+        }),
+        new webpack.optimize.ModuleConcatenationPlugin(),
+        new ProgressBarPlugin()
+    ]
+}
