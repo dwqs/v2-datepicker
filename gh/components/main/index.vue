e8bfd77c2172839642fda5b021432bb2c6748369
dwqs <461147874@qq.com>
2018-04-13 16:32:02 +0800

    refactor: Add demos

--- gh/components/main/index.vue
+++ gh/components/main/index.vue
@@ -4,11 +4,15 @@
         <h4>1. As the date-picker component/作为日期选择组件</h4>
         <h5>Default usage/默认形式</h5>
         <basic></basic>
+        <h5>Setting shortcuts/设置快捷项</h5>
+        <short-cuts></short-cuts>
         <h5>Change the language and the return form./更改语言和返回形式</h5>
         <basic-set></basic-set>
         <h4>2. As the daterange-picker component/作为日期区间选择组件</h4>
         <h5>Default usage/默认形式</h5>
         <basic-range></basic-range>
+        <h5>Setting shortcuts/设置快捷项</h5>
+        <rang-short-cuts></rang-short-cuts>
         <h5>Change the language and the return form./更改语言和返回形式</h5>
         <basic-range-set></basic-range-set>
         <h5>Disable the linkage./取消左右面板的日期选择联动</h5>
@@ -21,9 +25,11 @@
 
     import Basic from './date-picker/basic.vue';
     import BasicSet from './date-picker/basic-setting.vue';
+    import ShortCuts from './date-picker/shortcuts.vue';
     import BasicRange from './date-range/basic.vue';
     import BasicRangeSet from './date-range/basic-setting.vue';
     import DisableLink from './date-range/disable-link.vue';
+    import RangeShortCuts from './date-range/shortcuts.vue';
 
     export default {
         components: {
@@ -31,7 +37,9 @@
             BasicSet,
             BasicRange,
             BasicRangeSet,
-            DisableLink
+            DisableLink,
+            ShortCuts,
+            'rang-short-cuts': RangeShortCuts
         }
     };
 </script>
