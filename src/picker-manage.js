247be339de81e1f4c47fd0ec2ad0c3978e550889
dwqs <461147874@qq.com>
2018-04-13 15:19:26 +0800

    feat: Manage multi pickers in page

--- src/picker-manage.js
+++ src/picker-manage.js
@@ -1,37 +1,35 @@
 let pickerId = 0;
 
-class PickerManage {
-    constructor () {
-        this.pickers = [];
-    }
+function PickerManage () {
+    this.pickers = [];
+}
 
-    addPicker (type, picker) {
-        pickerId++;
-        this.pickers.push({
-            pid: pickerId,
-            type,
-            picker
-        });
-        return pickerId;
-    }
+PickerManage.prototype.addPicker = function (type, picker) {
+    pickerId++;
+    this.pickers.push({
+        pid: pickerId,
+        type,
+        picker
+    });
+    return pickerId;
+};
 
-    updatePicker (pid) {
-        this.pickers.forEach(item => {
-            if (item.pid !== pid) {
-                item.picker.shown = false;
-            }
-        });
-    }
+PickerManage.prototype.updatePicker = function (pid) {
+    this.pickers.forEach(item => {
+        if (item.pid !== pid) {
+            item.picker.shown = false;
+        }
+    });
+};
 
-    deletePicker (pid) {
-        this.pickers = this.pickers.filter(item => item.pid !== pid);
-        return -1;
-    }
+PickerManage.prototype.deletePicker = function (pid) {
+    this.pickers = this.pickers.filter(item => item.pid !== pid);
+    return -1;
+};
 
-    destroyed () {
-        this.pickers = [];
-        pickerId = 0;
-    }
+PickerManage.prototype.destroyed = function () {
+    this.pickers = [];
+    pickerId = 0;
 };
 
 const pickerManage = new PickerManage();
